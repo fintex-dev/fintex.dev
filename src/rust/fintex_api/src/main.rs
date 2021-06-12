075778b831e58c8c681b38753d6f06c1f9968401
mathias-dietrich <mdietric@redhat.com>
2021-06-12 09:45:17 +0200

    fix

--- src/rust/fintex_api/src/main.rs
+++ src/rust/fintex_api/src/main.rs
@@ -2,13 +2,10 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("api" / String)
+        .map(|name| format!("version 0.0.1 {}!", name));
 
-
-    let hello = warp::path!("api/version" / String)
-        .map(|name| format!("{'version':'0.0.1'}"));
-
-    let hello = warp::path!("api/echo" / String)
-        .map(|name| format!(" {}!", name));
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
